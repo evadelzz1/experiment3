d761b866095dd565818f44896d7340cdc46dbeb1
jkmathilda <jkmathilda627@gmail.com>
2024-01-20 15:36:26 -0500

    Update quizchart.py

--- quizchart.py
+++ quizchart.py
@@ -1,8 +1,70 @@
 import streamlit as st
+import os
 from dotenv import load_dotenv
 import pandas as pd
+from langchain_openai import OpenAI
+from PyPDF2 import PdfReader
+from langchain.prompts import PromptTemplate
+
+def initialize():
+    if 'q' not in st.session_state:
+        st.session_state.q = []
+
+def quizzer(pdf_text):
+    api_key = os.getenv("OPENAI_API_KEY")
+    llm = OpenAI(openai_api_key=api_key)
+    
+    prompt_template = PromptTemplate.from_template(
+        '''Provide a multiple-choice question with only ONE correct answer and the answer from this text: 
+        {pdf_file}
+        
+        Follow this template:
+            Question: (Question)
+            A. (multiple choice)
+            B. (multiple choice)
+            C. (multiple choice)
+            D. (multiple choice)
+            (Correct Answer)
+        
+        Example: 
+            Question: What run should you take to warm up on Blackcomb Mountain?
+            A. Easy Out
+            B. Countdown
+            C. Big Easy
+            D. Magic Castle
+            C. Big Easy
+        
+        '''
+    )
+    quiz = prompt_template.format(pdf_file=pdf_text)
+    
+    return llm(quiz)
+
+
+def quizzing(i, noq, pdf_text): # Takes care of a single question
+    
+    mcq = quizzer(pdf_text)
+    quiz = mcq.split('?')
+    
+    question = quiz[0][10:]
+    
+    # Only print out multiple choice portion
+    # [:-1] is used to omit 'A', 'B', 'C', 'D' in the end of each string
+    mc = quiz[1].split('.')
+    
+    mco = f'''A. {mc[1][:-1]} \nB.{mc[2][:-1]} \n{mc[3][:-1]} \n{mc[4][:-1]}'''
+    
+    # Correct answer
+    correct_ans = mc[5]
+    
+    return question, mco, correct_ans
+
+
+
 
 def main():
+    initialize()
+    
     # load api_key
     if not load_dotenv():
         st.info("Could not load .env file or it is empty. Please check if it exists and is readable.")
@@ -14,8 +76,49 @@ def main():
         page_icon="❔" 
     )
     
-    st.header('Quizzer❔')
+    st.header("Quizzer ❔")
     st.write('Quiz Generating App')
+    
+    noq = st.selectbox(
+            "Number of questions for your quiz:",
+            key='number_of_questions',
+            options=[5, 10, 15, 20, 25, 30]
+        )
+    
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    cancel_button = st.button('Cancel')
+
+    
+    if cancel_button:
+        st.stop()
+        
+    # List Of Questions, List of Multiple Choice Options, List of Correct Answers, List Of Index
+    loq, lmco, lca, loi = [], [], [], []
+        
+    if pdf is not None:
+        st.empty()
+        pdf_reader = PdfReader(pdf)
+        pdf_text = ""                       # Initialize a string to accumulate extracted text
+        for page in pdf_reader.pages:       # Loop through each page in the PDF
+            pdf_text += page.extract_text()
+            
+        for i in range(noq):
+            question, mco, correct_ans = quizzing(i, noq, pdf_text)
+            i += 1
+            loq.append(question)
+            lmco.append(mco)
+            lca.append(correct_ans)
+            loi.append(i)
+            
+    quizzes = {
+        "Question": loq,
+        "Multiple Choice Options": lmco,
+        "Correct Answer": lca
+    }
+    
+    quizchart = pd.DataFrame(quizzes, index = loi) # loi: list of i + 1
+    
+    st.dataframe(quizchart)
 
 
 if __name__ == '__main__':
